5e678eaa91c6696b4c43085aa04a8904834cb3d7
Felip3FL <felip3.fl@gmail.com>
2025-06-18 17:56:26 -0300

    New youtube file

---
+++ Material/Ico/youtube.ps1
@@ -0,0 +1,42 @@
+
+function youtube($originalUrl, $videoQuality = "2K") {
+
+    switch ($videoQuality.toupper()) {
+        ("1440","2K") { $videoQuality = "1440" }
+        ("1080","FULLHD") { $videoQuality = "1080" }
+        ("720","HD") { $videoQuality = "720" }
+        ("360","") { $videoQuality = "360" }
+        ("4k","2160") { $videoQuality = "2160" }
+        default { $videoQuality = "1440" }
+    }
+
+    echo "Video Quality: " $videoQuality
+
+    youtubeTemp
+
+    $url = $originalUrl
+    if([string]::IsNullOrEmpty($url)){
+        $url = Get-Clipboard
+    }
+
+	$urlWithoutParam = $url.Split("&")[0]
+
+    echo $urlWithoutParam
+
+    yt-dlp `
+        --check-formats `
+        --sponsorblock-remove sponsor,selfpromo `
+        -f "bestvideo[height<=$videoQuality]+(ba[format_note*=original]/ba)" `
+        -o '%(channel)s - %(title)s.%(ext)s' `
+        -P "temp:/mnt/d/Downloads" `
+        --all-subs `
+        --embed-subs `
+        --add-metadata `
+        $urlWithoutParam
+}
+
+# Set-Alias -Name youtube -Value youtube2k -Scope Global
+
+function youtubeTemp() {
+    cd 'D:\Videos\Youtube Temp\'
+}
