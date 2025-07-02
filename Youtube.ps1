a85bf9438770f0042d1cb33aa6f8845f833ec458
Felip3FL <felip3.fl@gmail.com>
2025-07-02 09:28:29 -0300

    Fix videoQuality

--- Youtube.ps1
+++ Youtube.ps1
@@ -1,16 +1,7 @@
 
-function youtube($originalUrl, $videoQuality = "2K") {
+function youtube($originalUrl, $videoQuality = "1440") {
 
-    switch ($videoQuality.toupper()) {
-        ("1440","2K") { $videoQuality = "1440" }
-        ("1080","FULLHD") { $videoQuality = "1080" }
-        ("720","HD") { $videoQuality = "720" }
-        ("360","") { $videoQuality = "360" }
-        ("4k","2160") { $videoQuality = "2160" }
-        default { $videoQuality = "1440" }
-    }
-
-    echo "Video Quality: " $videoQuality
+    echo "Video Quality: '$videoQuality'p" 
 
     youtubeTemp
 
@@ -35,14 +26,38 @@ function youtube($originalUrl, $videoQuality = "2K") {
         -f "bestvideo[height<=$videoQuality]+(ba[format_note*=original]/ba)" `
         -o '%(channel)s - %(title)s.%(ext)s' `
         -P "temp:$tempDownloadPath" `
-        --sub-langs 'enUS,en,en-US,pt,ptBR,pt-BR' `
-        --alias with-subs,--Xs '--X0 {0} --write-subs --embed-subs' `
-        --alias without-subs,--Xn '--X0 {0} --no-write-subs --no-embed-subs' `
+        --sub-langs 'enUS,en,en-US,pt,ptBR,pt-BR,br' `
+        --embed-subs `
+        --embed-thumbnail `
         --add-metadata `
         $urlWithoutParam
 }
 
-# Set-Alias -Name youtube -Value youtube2k -Scope Global
+function youtube2k($originalUrl) {
+    youtube $originalUrl "1440"
+}
+
+function youtube1440($originalUrl) {
+    youtube $originalUrl "1440"
+}
+
+function youtubefullhd($originalUrl) {
+    youtube $originalUrl "1080"
+}
+
+function youtubehd($originalUrl) {
+    youtube $originalUrl "720"
+}
+
+function youtube360($originalUrl) {
+    youtube $originalUrl "360"
+}
+
+function youtube4k($originalUrl) {
+    youtube $originalUrl "2160"
+}
+
+# Set-Alias -Name youtube2k -Value youtube -Scope Global
 
 function youtubeTemp() {
     if($IsLinux){
